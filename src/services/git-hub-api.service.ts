3da3853b8006f055cf2ac4cd531b130fcc5623b5
Dario Djuric <dario.djuric@gmail.com>
2021-07-23 21:15:01 +0200

    feat: add github service

---
+++ src/services/git-hub-api.service.ts
@@ -0,0 +1,28 @@
+import {inject, Provider} from '@loopback/core';
+import {getService} from '@loopback/service-proxy';
+import {GitHubApiDataSource} from '../datasources';
+
+export class GitHubIssueResponse {
+  issueNumber: number;
+  title: string;
+}
+
+export interface GitHubApi {
+  getIssue(
+    repositoryOwner: string,
+    repositoryName: string,
+    issueNumber: number,
+  ): Promise<GitHubIssueResponse>;
+}
+
+export class GitHubApiProvider implements Provider<GitHubApi> {
+  constructor(
+    // GitHubApi must match the name property in the datasource json file
+    @inject('datasources.GitHubApi')
+    protected dataSource: GitHubApiDataSource = new GitHubApiDataSource(),
+  ) {}
+
+  value(): Promise<GitHubApi> {
+    return getService(this.dataSource);
+  }
+}
---
+++ src/services/index.ts
@@ -0,0 +1,1 @@
+export * from './git-hub-api.service';
